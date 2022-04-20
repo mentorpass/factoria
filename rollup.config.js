6031e3d7ae1be96b21524ee99bd1a886ba9b9d8e
LawsonTaylor <lawson.j.taylor@gmail.com>
2022-04-20 15:45:27 -0500

    fix(rollup.config & .gitignore): make faker external, add /build to repo

--- rollup.config.js
+++ rollup.config.js
@@ -1,29 +1,26 @@
-import typescript from '@rollup/plugin-typescript'
-import nodeResolve from '@rollup/plugin-node-resolve'
-import commonjs from '@rollup/plugin-commonjs'
-import { terser } from 'rollup-plugin-terser'
+import typescript from "@rollup/plugin-typescript";
+import nodeResolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import { terser } from "rollup-plugin-terser";
 
 export default {
-  input: 'index.ts',
-  context: 'this',
-  external: ['faker'],
+  input: "index.ts",
+  context: "this",
+  external: ["@faker-js/faker"],
   output: [
     {
-      entryFileNames: 'factoria.min.js',
-      dir: 'dist',
-      format: 'cjs',
-      exports: 'auto',
-      plugins: [terser()]
-    }, {
-      entryFileNames: 'factoria.js',
-      dir: 'dist',
-      format: 'cjs',
-      exports: 'auto'
-    }
+      entryFileNames: "factoria.min.js",
+      dir: "dist",
+      format: "cjs",
+      exports: "auto",
+      plugins: [terser()],
+    },
+    {
+      entryFileNames: "factoria.js",
+      dir: "dist",
+      format: "cjs",
+      exports: "auto",
+    },
   ],
-  plugins: [
-    typescript(),
-    commonjs(),
-    nodeResolve()
-  ]
-}
+  plugins: [typescript(), commonjs(), nodeResolve()],
+};
